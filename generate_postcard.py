106a5354270aa798d5b5943295c28397a2264926
Gabriel Sinkin <gabriel.sinkin@gmail.com>
2017-08-28 13:29:49 -0700

    Generate postcard from random gifs

--- generate_postcard.py
+++ generate_postcard.py
@@ -1,19 +1,52 @@
 import os
+import json
+import urllib2
+import logging
 from cStringIO import StringIO
 
+from PIL import Image
 import lob
+from reportlab.lib import utils
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 
+logger = logging.getLogger()
 lob.api_key = os.environ["LOB_API_KEY"]
+GIPHY_API_KEY = os.environ["GIPHY_API_KEY"]
+
+
+def get_random_image():
+    logger.debug("Getting random image")
+    request = urllib2.Request(
+        "https://api.giphy.com/v1/gifs/random?api_key={0}"
+        "&tag=&rating=PG-13".format(GIPHY_API_KEY))
+    response = urllib2.urlopen(request).read()
+    loaded = json.loads(response)
+    image_data = loaded["data"]
+    return {
+        "image_io": StringIO(
+            urllib2.urlopen(image_data["image_original_url"]).read()
+        ),
+        "width": image_data["image_width"],
+        "height": image_data["image_height"],
+    }
 
 
 def generate_pdf():
+    image = get_random_image()
+    logger.debug("Generating pdf")
     result = StringIO()
     height, width = (4.25 * inch, 6.25 * inch)
     pdf_canvas = canvas.Canvas(result, pagesize=(width, height))
-    pdf_canvas.setFont("Helvetica-Bold", 16)
-    pdf_canvas.drawString(100, 100, "Hello World")
+    pil_image = Image.open(image["image_io"])
+    image["image_io"].seek(0)
+    img = utils.ImageReader(image["image_io"])
+    iw, ih = img.getSize()
+    width = 2 * inch
+    aspect = ih / float(iw)
+    pdf_canvas.drawInlineImage(
+        pil_image, 0.5 * inch, 0.5 * inch,
+        width=width, height=(width * aspect))
     pdf_canvas.showPage()
     pdf_canvas.save()
     result.seek(0)
@@ -21,6 +54,7 @@ def generate_pdf():
 
 
 def generate_postcard(front_pdf, back_pdf):
+    logger.debug("Generating postcard")
     postcard = lob.Postcard.create(
         to_address={
             'name': 'Kate Cryan',
